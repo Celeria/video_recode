07a9d36177d01a8893105f26084b25634f3a216f
Patrick <patm314@gmail.com>
2024-10-03 14:20:23 -0400

    initial generation with this prompt
    I have a directory that only contains video files in mp4 format. They are all dashcam footages, and the filename is the exact start time of the video. 2024_0921_222828_010 is one example of such a filename (likely has a .mp4 at the end if you are directly accessing directory files via code). The last underscore with a number is just the next video recorded in series, let's ignore that number. (_010 in this case). I want a copy of merged videos (with proper metadata reflecting new longer lengths) with "the same trip" being combined together instead of remaining as separate files. I am trying to write a complete python script that will do a "basic copy and paste" from the directory that contains my original files, to another directory. I have provided the paths below, leave them at the beginning of the script for simplicity:
    
    # Hardcoded directories
    
    source_dir = r"D:\Patrick's Documents\Other Things\Video\Dashcam\Hawaii Trip"
    
    output_dir = r"D:\Patrick's Documents\Other Things\Video\Dashcam\concatenated_output" 
    
    Here are the steps that you can use to place files into my output_dir.
    
    First, read through every mp4 file in that folder, and figure out the time of creation of each one. You can assume they will be sequential, if they are not, sort them from oldest to newest. Create a "user defined hardcoded variable" for "maximum length in minutes of each video". Let's prefill it with 4 minutes, (since most videos are 3 minutes long, but let's make it flexible to change at the top in case this changes). Starting from the first video, check when the next video in the sequence was created. If that time is LESS than the video_length_max defined by the user, add it to sequence. Go to that video, and repeat the process on the next one in the sequence. If the next video takes place at a time LONGER than that, it is a new session. For each merged video, title it the same as the first video in the sequence.

--- video_merger_v2.py
+++ video_merger_v2.py
@@ -4,4 +4,63 @@ Created on Thu Oct  3 13:57:20 2024
 
 @author: patm3
 """
+import os
+import datetime
 
+# Hardcoded directories (use absolute paths)
+source_dir = r"D:\Patrick's Documents\Other Things\Video\Dashcam\Hawaii Trip"
+output_dir = r"D:\Patrick's Documents\Other Things\Video\Dashcam\concatenated_output"
+
+# Maximum length of each video in minutes
+video_length_max = 4
+
+def get_video_time(filename):
+  """Extracts the date and time from the filename and returns a datetime object."""
+  try:
+    date_time_str = filename.split('_')[0] + filename.split('_')[1]
+    return datetime.datetime.strptime(date_time_str, '%Y%m%d%H%M%S')
+  except:
+    print(f"Error parsing filename: {filename}")
+    return None
+
+def concatenate_videos(video_list, output_filename):
+  """Concatenates the videos in the list and saves the result to the output filename."""
+  with open("mylist.txt", "w") as f:
+    for video in video_list:
+      f.write(f"file '{os.path.abspath(video)}'\n") # Use absolute path here
+  os.system(f'ffmpeg -f concat -safe 0 -i mylist.txt -c copy "{output_filename}"')
+  os.remove("mylist.txt")
+
+def main():
+  """Main function to process the video files."""
+  video_files = [f for f in os.listdir(source_dir) if f.endswith('.mp4')]
+  video_files.sort(key=get_video_time)
+
+  current_sequence = []
+  start_time = None
+
+  for i, video_file in enumerate(video_files):
+    video_time = get_video_time(video_file)
+    if video_time is None:
+      continue
+
+    if not current_sequence:
+      current_sequence.append(os.path.join(os.path.abspath(source_dir), video_file))  # Use absolute path here
+      start_time = video_time
+    else:
+      time_diff = video_time - start_time
+      if time_diff <= datetime.timedelta(minutes=video_length_max):
+        current_sequence.append(os.path.join(os.path.abspath(source_dir), video_file)) # Use absolute path here
+      else:
+        output_filename = os.path.join(output_dir, f"{os.path.splitext(os.path.basename(current_sequence[0]))[0]}.mp4")
+        concatenate_videos(current_sequence, output_filename)
+        current_sequence = [os.path.join(os.path.abspath(source_dir), video_file)] # Use absolute path here
+        start_time = video_time
+
+    # Concatenate the last sequence
+    if i == len(video_files) - 1:
+      output_filename = os.path.join(output_dir, f"{os.path.splitext(os.path.basename(current_sequence[0]))[0]}.mp4")
+      concatenate_videos(current_sequence, output_filename)
+
+if __name__ == "__main__":
+  main()
